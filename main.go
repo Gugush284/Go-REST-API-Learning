06c0ee65644057fffd866a7470a0ea683a9f471d
Gugush284 <gugush284@gmail.com>
2022-06-23 16:12:24 +0300

    Created a simple 'hello world' site

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-    println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+	http.ListenAndServe(":80", nil)
 }
