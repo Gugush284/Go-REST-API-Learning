040f56b18987e4dac46861856c8663daaa4e2884
Gugush284 <gugush284@gmail.com>
2022-06-30 13:01:03 +0300

    Create a Handle Function

--- main.go
+++ main.go
@@ -5,9 +5,27 @@ import (
 	"net/http"
 )
 
+type Book struct {
+	ID    int
+	Title string
+}
+
+var book []Book
+
+func getid(w http.ResponseWriter, r *http.Request) {
+	for i := 0; i < len(book); i++ {
+		fmt.Fprintf(w, "%d - %s\n", book[i].ID, book[i].Title)
+	}
+}
+
+func Read_db() {
+	book = append(book, Book{1, "Tom"})
+	book = append(book, Book{2, "gfg"})
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
+	Read_db()
+
+	http.HandleFunc("/", getid)
 	http.ListenAndServe(":80", nil)
 }
