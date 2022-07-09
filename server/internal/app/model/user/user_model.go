f44e10064d88bbd91a6fc0ec0ea6f0782269c582
Gugush284 <gugush284@gmail.com>
2022-07-09 21:47:52 +0300

    Add password encryption method

--- server/internal/app/model/user/user_model.go
+++ server/internal/app/model/user/user_model.go
@@ -1,12 +1,38 @@
 package model_user
 
+import "golang.org/x/crypto/bcrypt"
+
 // User struct ...
 type User struct {
-	ID       int
-	Login    string
-	Password string
+	ID                int
+	Login             string
+	Password          string
+	DecryptedPassword string
 }
 
 func New() *User {
 	return &User{}
+}
+
+// Before Create user in database
+func (u *User) PreparationCreate() error {
+	if len(u.DecryptedPassword) > 0 {
+		enc, err := encryptString(u.DecryptedPassword)
+		if err != nil {
+			return err
+		}
+
+		u.Password = enc
+	}
+
+	return nil
+}
+
+func encryptString(str string) (string, error) {
+	b, err := bcrypt.GenerateFromPassword([]byte(str), bcrypt.MinCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(b), err
 }
