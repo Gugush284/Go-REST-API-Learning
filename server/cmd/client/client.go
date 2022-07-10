abb8f719bcfe104967f73c8ce1ece446200c2f12
Gugush284 <gugush284@gmail.com>
2022-07-10 21:08:19 +0300

    Upgrade client

--- server/cmd/client/client.go
+++ server/cmd/client/client.go
@@ -3,15 +3,70 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	MakeRequest()
+	Menu()
+
+	for {
+		var key int
+
+		fmt.Fprint(os.Stdout, "Choose func\n")
+		fmt.Fscan(os.Stdin, &key)
+
+		switch key {
+		case 0:
+			os.Exit(0)
+		case 1:
+			CreateRequest()
+		case 2:
+			SessionRequest()
+		case 3:
+			Menu()
+		default:
+			fmt.Fprint(os.Stdout, "Unknown case\n")
+		}
+	}
 }
 
-func MakeRequest() {
+func Menu() {
+	fmt.Fprint(os.Stdout, "Menu:\n")
+	fmt.Fprint(os.Stdout, " case 0: Exit\n")
+	fmt.Fprint(os.Stdout, " case 1: CreateRequest\n")
+	fmt.Fprint(os.Stdout, " case 2: SessionRequest\n")
+	fmt.Fprint(os.Stdout, " case 3: Menu\n")
+}
+
+func SessionRequest() {
+
+	message := map[string]interface{}{
+		"login":    "Slava8924",
+		"password": "JGjkgkggd9",
+	}
+
+	bytesRepresentation, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post("http://localhost:8080/sessions", "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["data"])
+}
+
+func CreateRequest() {
 
 	message := map[string]interface{}{
 		"login":    "Slava8924",
