ada95603e9f5bcc98bf589d8a2c5e6dc680c9ad6
Gugush284 <gugush284@gmail.com>
2022-07-11 19:06:10 +0300

    Change errors.go to constants.go

---
+++ server/internal/apiserver/constants.go
@@ -0,0 +1,18 @@
+package Constants
+
+import (
+	"errors"
+)
+
+type ctxKey int8
+
+var (
+	ErrRecordNotFound           = errors.New("record not found")
+	ErrIncorrectLoginOrPassword = errors.New("incorrect login or password")
+	ErrNotAuthenticated         = errors.New("not authenticated")
+)
+
+const (
+	SessionName        = "activesession"
+	CtxKeyUser  ctxKey = iota
+)
--- server/internal/apiserver/errors.go
+++
@@ -1,8 +0,0 @@
-package globalErrors
-
-import "errors"
-
-var (
-	ErrRecordNotFound           = errors.New("record not found")
-	ErrIncorrectLoginOrPassword = errors.New("incorrect login or password")
-)
