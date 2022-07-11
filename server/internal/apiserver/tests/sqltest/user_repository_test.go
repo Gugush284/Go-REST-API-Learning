ee2d626e8b4531a2ae6659c49d8a3d505407d05c
Gugush284 <gugush284@gmail.com>
2022-07-11 16:31:48 +0300

    Add <Find by id> test

--- server/internal/apiserver/tests/sqltest/user_repository_test.go
+++ server/internal/apiserver/tests/sqltest/user_repository_test.go
@@ -21,7 +21,7 @@ func TestUserRepository_Create(t *testing.T) {
 	assert.NotNil(t, u)
 }
 
-func TestUserRepository_Find(t *testing.T) {
+func TestUserRepository_FindByLogin(t *testing.T) {
 	s, teardown := TestStore(t, dbURL)
 	defer teardown("users")
 
@@ -50,6 +50,33 @@ func TestUserRepository_Find(t *testing.T) {
 	username, err := s.User().FindByLogin(user.Login)
 	/*if username != nil {
 		fmt.Println("4", username.ID, username.Login, username.DecryptedPassword, username.Password)
+	}*/
+	assert.NoError(t, err)
+	assert.NotNil(t, username)
+}
+
+func TestUserRepository_Find(t *testing.T) {
+	s, teardown := TestStore(t, dbURL)
+	defer teardown("users")
+
+	u := ModelUserTest.TestUser(t)
+	/*if user != nil {
+		fmt.Println("1", user.ID, user.Login, user.DecryptedPassword, user.Password)
+	}*/
+
+	u, err := s.User().Create(&model_user.User{
+		Login:             u.Login,
+		DecryptedPassword: u.DecryptedPassword,
+	})
+	/*if u != nil {
+		fmt.Println("2", u.ID, u.Login, u.DecryptedPassword, u.Password)
+	}*/
+	assert.NoError(t, err)
+	assert.NotNil(t, u)
+
+	username, err := s.User().Find(u.ID)
+	/*if username != nil {
+		fmt.Println("3", username.ID, username.Login, username.DecryptedPassword, username.Password)
 	}*/
 	assert.NoError(t, err)
 	assert.NotNil(t, username)
